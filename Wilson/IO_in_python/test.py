b55e894381dff8aad4bbfc16d2e5241ac94f101b
Huang, Po-Hsiang <yak30257987@gmail.com>
2024-07-08 00:01:34 +0800

    Created WorkRelated Folder and added sending email with attachments to Outlook function

--- Wilson/IO_in_python/test.py
+++
@@ -1,9 +0,0 @@
-# 定義一個包含字符串的列表
-names = ["John", "Jane", "Alice", "Bob"]
-
-# 使用 sort() 方法按字母逆序排序
-names.sort(key=lambda x: x[::-1])
-
-# 打印按字母逆序排序後的列表
-print("按字母逆序排序後的列表:", names)
-# 輸出: 按字母逆序排序後的列表: ['John', 'Jane', 'Alice', 'Bob']
